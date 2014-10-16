a0e717cebccf5c04332655ae0086bae819a62421
Matt Innes <matt@kali.lan>
2014-10-16 14:53:56 +0100

    Converted multimethod to protocol for performance reasons and added code to recursively stringify ObjectIds (for, say, JSON encoding)

--- src/com/madeye/clojure/common/mongo.clj
+++ src/com/madeye/clojure/common/mongo.clj
@@ -4,17 +4,49 @@
     [monger.core :as mg]
     [monger.collection :as mc]
     [com.madeye.clojure.common.config :as cfg]
+    [clojure.walk]
   )
   (:import [org.bson.types ObjectId])
 )
 
-(defmulti get-id
-  "Multimethod to allow us to treat stringified object ids the same as object ids"
-  (fn [id] (class id))
+(defn reload [] (use :reload-all 'com.madeye.clojure.common.mongo))
+
+; Multi-methods are much slower than protocols so ...
+; (defmulti get-id
+;   "Multimethod to allow us to treat stringified object ids the same as object ids"
+;   (fn [id] (class id))
+; )
+; 
+; (defmethod get-id java.lang.String [id] (ObjectId. id))
+; (defmethod get-id org.bson.types.ObjectId [id] id)
+
+(defprotocol GetId
+  (get-id [id])
+  (get-str [id])
 )
 
-(defmethod get-id java.lang.String [id] (ObjectId. id))
-(defmethod get-id org.bson.types.ObjectId [id] id)
+(extend-protocol GetId java.lang.String
+  (get-id [id] (ObjectId. id))
+  (get-str [id] id)
+)
+(extend-protocol GetId org.bson.types.ObjectId 
+  (get-id [id] id)
+  (get-str [id] (str id))
+)
+(extend-protocol GetId Object
+  (get-id [obj] obj)
+  (get-str [obj] obj)
+)
+
+(defn objectid?
+  [id]
+  (= (class id) org.bson.types.ObjectId)
+)
+
+(defn object-id-to-str
+  [m]
+  (clojure.walk/postwalk #(if (objectid? %) (get-str %) %) m)
+)
 
 (defn initialise
   []
